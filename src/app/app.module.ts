3b46e4a47bbf1bd9d1e3deb2a2bc52fc9db3da2b
Chintu Sitlani <chintusitlani09@gmail.com>
2025-03-31 22:42:11 +0530

    service component folder correected and added buyer service added buyer and seller component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 import { SignupComponent } from "./signup/signup.component";
-import { SellerService } from "./servies/seller.service";
 import { NgModule } from "@angular/core";
-
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 
 @NgModule({
   declarations: [
--- src/app/services/buyer.service.ts
+++ src/app/services/buyer.service.ts
@@ -1,19 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { sellerData } from '../data-type';
 import { environment } from '../../environments/environments';
-import { error } from 'console';
+import { HttpClient } from '@angular/common/http';
+import { userData } from '../data-type';
 import { throwError } from 'rxjs/internal/observable/throwError';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SellerService {
+export class BuyerService {
 
   private baseUrl = environment.apiUrl;
   constructor(private http: HttpClient) { }
 
-  userSignup(data: sellerData, param: string) {
+  buyerSignup(data: userData, param: string) {
     if (param !== '') {
       return this.http.post(`${this.baseUrl}/${param}`, data);
     }
