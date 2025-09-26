cb913707a74dd175bc0af7851b78449e0d54b249
chintusitlani <chintusitlani09@gmail.com>
2025-09-26 23:06:14 +0530

    feat: add top header component with responsive design and styling; integrate into header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { TopHeaderComponent } from '../top-header/top-header.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, NavbarComponent],
+  imports: [CommonModule, NavbarComponent,TopHeaderComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
