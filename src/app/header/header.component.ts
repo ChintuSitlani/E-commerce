27638f59073119665b61d3dc1f5f40fab70914df
chintusitlani <chintusitlani09@gmail.com>
2025-09-29 20:25:31 +0530

    feat: integrate sub-header component into header; update styles and layout for top header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { TopHeaderComponent } from '../top-header/top-header.component';
+import { SubHeaderComponent } from '../sub-header/sub-header.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, NavbarComponent,TopHeaderComponent],
+  imports: [CommonModule, NavbarComponent,TopHeaderComponent,SubHeaderComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
